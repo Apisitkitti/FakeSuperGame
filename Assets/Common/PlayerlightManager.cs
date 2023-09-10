3524a1cf28019c9fac29158522f549d890efd39e
Fennikz <tripop.xtarnos@mail.kmutt.ac.th>
2023-09-10 22:15:30 +0700

    Add:playerlight

--- Assets/Common/PlayerlightManager.cs
+++ Assets/Common/PlayerlightManager.cs
@@ -5,32 +5,28 @@ using PhEngine.Core;
 using UnityEngine.Rendering.Universal;
 namespace SuperGame
 {
-    public class TimeManager : MonoBehaviour
-    {
-         
-        [SerializeField] Light2D LightSource;
+    public class PlayerlightManager: MonoBehaviour
+    {   
+        [SerializeField] Light2D PlayerLight;
 
         void Update()
         {
            TimeCheck();
         }
-        
+
        void TimeCheck()
         {
              if(WeatherManager.Instance.TimeManager == 1)
             {
-                LightSource.color = new Color(1f,0.7375f,0.6650f,1f);
-               
+                PlayerLight.enabled = false;
             }
             if(WeatherManager.Instance.TimeManager == 2)
             {
-                LightSource.color = new Color(1f,1f,1f,1f);
-           
+                PlayerLight.enabled = false;
             }
             if(WeatherManager.Instance.TimeManager == 3)
             {
-                LightSource.color = new Color(0.2404522f,0.1950872f,0.3018868f,1f);
-     
+                PlayerLight.enabled = true;
             }
         }
     }
